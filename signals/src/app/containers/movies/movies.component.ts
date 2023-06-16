316e9dff1c012876dc298f579756dbf368f0fb6a
KonradCzerwinski <kondi.czerwinski@gmail.com>
2023-06-16 13:53:06 +0100

    first commit

--- signals/src/app/containers/movies/movies.component.ts
+++ signals/src/app/containers/movies/movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal, untracked } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,9 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MoviesService } from '../../services/movies.service';
-import {map, Observable, of} from 'rxjs';
 import { MovieComponent } from '../../components/movie/movie.component';
-import {Movie} from "../../models/movie.interface";
 
 @Component({
   selector: 'app-movies',
@@ -28,13 +26,32 @@ import {Movie} from "../../models/movie.interface";
 export class MoviesComponent {
   private moviesService: MoviesService = inject(MoviesService);
 
-  title = '';
-  year = '';
+  title = signal('');
+  year = signal('');
+  anotherSignal = signal(0);
 
-  movies$: Observable<Movie[]> = of([]);
+  performedSearch = computed(
+    () =>
+      'Performed Search: ' + untracked(this.title) + ', Year: ' + this.year()
+  );
 
-  search() {
-    this.movies$ = this.moviesService.searchMovies(this.title, this.year);
+  movies = signal([]);
+
+  constructor() {
+    // effect(() => {
+    //   this.moviesService.searchMovies(untracked(this.title), this.year()).subscribe();
+    // });
   }
 
+  async search() {
+    const movies = await this.moviesService.searchMovies(
+      this.title(),
+      this.year()
+    );
+    this.movies.set(movies);
+  }
+
+  updateTitle() {
+    this.movies.update((movies) => [{...movies[0], Title: 'New Title'}, ...movies.slice(1)]);
+  }
 }
--- signals/src/app/services/movies.service.ts
+++ signals/src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {map, Observable} from "rxjs";
+import {firstValueFrom, map, Observable, Subject, tap} from "rxjs";
 import {Movie} from "../models/movie.interface";
 
 @Injectable({
@@ -10,10 +10,12 @@ export class MoviesService {
   omdapi = 'https://www.omdbapi.com/';
   omdapi_apikey = '83513884';
 
+  movies$: Subject<Movie[]> = new Subject<Movie[]>();
+
   private httpClient: HttpClient = inject(HttpClient);
 
-  searchMovies(title: string, year: string): Observable<Movie[]> {
-    return this.httpClient.get<any>(`${this.getUrl()}s=${title}${year ? `&y=${year}` : ''}`).pipe(map((data: any) => data.Search));
+  searchMovies(title: string, year: string): Promise<Movie[]> {
+    return firstValueFrom(this.httpClient.get<any>(`${this.getUrl()}s=${title}${year ? `&y=${year}` : ''}`).pipe(map((data: any) => data.Search), tap((movies: Movie[]) => this.movies$.next(movies))));
   }
 
   private getUrl(): string {
